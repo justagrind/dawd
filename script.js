8834da403124aee16bcbb7373cc10f145b3143c8
justagrind <agarwalsanyam662@gmail.com>
2025-06-08 19:47:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const pages = document.querySelectorAll('.page');
 let currentPage = 0;
 
+// Set initial z-index values to stack the pages
+pages.forEach((page, index) => {
+  page.style.zIndex = pages.length - index;
+});
+
 function updatePageNumber() {
   document.getElementById('pageNumber').textContent = currentPage + 1;
 }
 
 function nextPage() {
-  if (currentPage < pages.length - 1) {
+  if (currentPage < pages.length) {
     pages[currentPage].classList.add('flipped');
+    pages[currentPage].style.zIndex = currentPage; // Send behind
     currentPage++;
     updatePageNumber();
   }
@@ -17,9 +23,9 @@ function prevPage() {
   if (currentPage > 0) {
     currentPage--;
     pages[currentPage].classList.remove('flipped');
+    pages[currentPage].style.zIndex = pages.length - currentPage; // Bring forward again
     updatePageNumber();
   }
 }
 
 updatePageNumber();
-
